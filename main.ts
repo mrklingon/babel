b6e47bd31289d8d5dd5123aa6f8c1c6cf9a02dc0
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-12-22 15:06:11 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,11 +22,15 @@ function consonant (text: string) {
     return text.charAt(randint(0, text.length - 1))
 }
 input.onButtonPressed(Button.A, function () {
+    music.playTone(523, music.beat(BeatFraction.Quarter))
     lang += 1
-    if (lang > 5) {
+    if (lang > 6) {
         lang = 0
     }
     basic.showString("" + (langs[lang]))
+    if (lang == 6) {
+        MakeX()
+    }
 })
 function Krule () {
     return Krules[randint(0, Krules.length - 1)]
@@ -35,9 +39,13 @@ function vrule () {
     return vulr[randint(0, vulr.length - 1)]
 }
 input.onButtonPressed(Button.AB, function () {
-    basic.showString(NewWord)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    basic.showString("" + (NewWord))
 })
 input.onButtonPressed(Button.B, function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
     if (0 == lang) {
         basic.showString("" + (Make_Word(Krule(), Kconst, Kvowel)))
     }
@@ -56,9 +64,38 @@ input.onButtonPressed(Button.B, function () {
     if (3 == lang) {
         basic.showString("" + (Make_Word(vrule(), vulc, vulv)))
     }
+    if (6 == lang) {
+        basic.showString("" + (Make_Word(Xrule(), Xcon, Xvow)))
+    }
 })
+function Thinking () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
 input.onGesture(Gesture.Shake, function () {
-    basic.showString(SNT)
+    music.playTone(262, music.beat(BeatFraction.Eighth))
+    basic.showString("" + (SNT))
 })
 function nextword () {
     if (0 == lang) {
@@ -79,22 +116,52 @@ function nextword () {
     if (3 == lang) {
         basic.showString("" + (Make_Word(vrule(), vulc, vulv)))
     }
+    if (6 == lang) {
+        basic.showString("" + (Make_Word(Xrule(), Xcon, Xvow)))
+    }
 }
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    music.playMelody("C D E F G F E C ", 120)
     SNT = ""
     for (let index = 0; index < randint(2, 5); index++) {
         nextword()
         SNT = "" + SNT + " " + NewWord
     }
     SNT = "" + SNT + Punct.substr(randint(0, Punct.length - 1), 1)
-    basic.showString(SNT)
+    basic.showString("" + (SNT))
 })
 function erule () {
     return engr[randint(0, engr.length - 1)]
 }
+function MakeX () {
+    Xvlist = ["eeeeeeeaaaaaaaaa", "aaaaeeeeiiiooooouuuyy", "oooooaaaaaaaa"]
+    XClist = ["grgggrrrqqqkkkpppxxx", "llllssrrrmmmnnnzzzz", "bptbbbtttpkkjjj"]
+    Xvow = Xvlist[randint(0, 2)]
+    Xcon = XClist[randint(0, 2)]
+    Thinking()
+    Xrules = []
+    for (let index = 0; index <= randint(3, 5); index++) {
+        xrx = ""
+        for (let index2 = 0; index2 <= randint(3, 5); index2++) {
+            if (randint(0, 4) > 2) {
+                xrx = "" + xrx + "C"
+            } else {
+                xrx = "" + xrx + "V"
+            }
+            Xrules.push(xrx)
+        }
+    }
+    Thinking()
+}
 function mrule () {
     return mandor[randint(0, mandor.length - 1)]
 }
+function Xrule () {
+    return Xrules[randint(0, Xrules.length - 1)]
+}
+let xrx = ""
+let XClist: string[] = []
+let Xvlist: string[] = []
 let SNT = ""
 let NewWord = ""
 let wookr: string[] = []
@@ -117,6 +184,9 @@ let Krules: string[] = []
 let Kvowel = ""
 let Kconst = ""
 let lang = 0
+let Xrules: string[] = []
+let Xvow = ""
+let Xcon = ""
 let langs: string[] = []
 images.createBigImage(`
     # . # . . . . # . .
@@ -132,7 +202,10 @@ images.createBigImage(`
     . . . . # . . . . .
     . . . . # . . . . .
     `).scrollImage(1, 200)
-langs = ["K", "E", "R", "V", "W", "M"]
+langs = ["K", "E", "R", "V", "W", "M", "X"]
+Xcon = ""
+Xvow = ""
+Xrules = [" "]
 lang = 0
 Kconst = "bcDgHjlmnpqQStvwy'''"
 Kvowel = "aeouyI"
@@ -153,6 +226,3 @@ vulr = ["CVCVCV", "CVCV", "CCV", "CVVCV", "CVVCCVCV"]
 wookv = "OUA"
 wookc = "WWRRRHHHWWWRR"
 wookr = ["CVVVVC", "CVC", "VVCV", "VCVVVVC", "VCVVC"]
-basic.forever(function () {
-	
-})
